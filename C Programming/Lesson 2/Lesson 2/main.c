7e37df44fc58947a89950ab87839c1b0d9e4a94c
Michele Galvagno <michicello@gmail.com>
2021-05-23 16:14:29 +0200

    New test

--- C Programming/Lesson 2/Lesson 2/main.c
+++ C Programming/Lesson 2/Lesson 2/main.c
@@ -20,3 +20,4 @@ int main(int argc, const char * argv[]) {
  comment! */
 
 // Testing a change
+// Testing a second change
---
+++ C Programming/Lesson X (to delete)/Lesson X (to delete)/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lesson X (to delete)
+//
+//  Created by Michele Galvagno on 23/05/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

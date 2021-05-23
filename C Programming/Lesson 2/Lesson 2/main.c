155153f534c8547e54eeb03f91633758aeb6eaa2
Michele Galvagno <michicello@gmail.com>
2021-05-23 15:24:50 +0200

    Adding Lesson 2 & 3 updates
    The file is always and only the same C file, comments show what has changed between lessons.

---
+++ C Programming/Lesson 2/Lesson 2/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  Lesson 2
+//
+//  Created by Michele Galvagno on 23/05/21.
+//
+
+#include <stdio.h> // Standard Input-Output (Library)
+
+// The main function is what the computer runs first
+int main(int argc, const char * argv[]) {
+    // insert code here... <-- this is a one-line comment
+    printf("Hello, World!\n"); // a function that gives output to the console
+    printf("Second Line\n");
+    return 0; // when the program's done
+}
+
+/* This is a
+ multiple line
+ comment! */

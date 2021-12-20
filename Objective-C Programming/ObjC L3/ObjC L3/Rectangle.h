105056dfd76786745ba534408287e3c3eea3285e
Michele Galvagno <michicello@gmail.com>
2021-12-20 21:46:30 +0100

    Lesson 3 on @interface files

--- Objective-C Programming/ObjC L3/ObjC L3/Rectangle.h
+++ Objective-C Programming/ObjC L3/ObjC L3/Rectangle.h
@@ -2,14 +2,24 @@
 //  Rectangle.h
 //  ObjC L3
 //
-//  Created by Michele Galvagno on 18/12/21.
+//  Created by Michele Galvagno on 20/12/21.
 //
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@protocol Rectangle <NSObject>
+// no curly braces in the new implementation
+@interface Rectangle : NSObject {
+    // instance variables
+    int height;
+    int width;
+}
+
+// two types of methods, instance method and class method. By now use -
+- (void)setHeight:(int)newHeight;
+- (void)setWidth:(int)newWidth; // these are like function prototypes
+- (void)setHeight:(int)newHeight width:(int)newWidth; 
 
 @end
 
--- Objective-C Programming/ObjC L3/ObjC L3/Rectangle.m
+++ Objective-C Programming/ObjC L3/ObjC L3/Rectangle.m
@@ -2,7 +2,7 @@
 //  Rectangle.m
 //  ObjC L3
 //
-//  Created by Michele Galvagno on 18/12/21.
+//  Created by Michele Galvagno on 20/12/21.
 //
 
 #import "Rectangle.h"
